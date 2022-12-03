ce357248567026c738a439a4d9b8a620f2b8a91b
Aleksandr <graftapisha@gmail.com>
2022-12-03 21:34:01 +0500

    initial

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100;
+        // начальная сумма счета
+        int pay = 2345;
+        // сумма пополнения
+        int bonus;
+        if (pay > 1000) {
+            bonus = pay / 100;
+            // бонус за каждые 100 р с суммы свыше 1000 р
+        } else {
+            bonus = 0;
+        }
+        int invoice_amount = amount + pay + bonus;
+        System.out.println("На вашем счету: " + invoice_amount);
+    }
+}
